b189f7170883fd3f0fd3eaf42242e9514f15e74c
Four26 <87604298+Four26@users.noreply.github.com>
2022-10-02 20:22:58 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+const dynamicContent = document.getElementById("dynamic-text");
+
+const phrases = ["Hello!"];
+
+let phraseIndex = 0;
+let letterIndex = 0;
+const typingSpeed = 700;
+const erasingSpeed = 400;
+
+function printLetters(phrase) {
+  if (letterIndex == phrase.length) {
+    clearLetters();
+  } else if (letterIndex < phrase.length) {
+    dynamicContent.textContent += phrase.charAt(letterIndex);
+    letterIndex += 1;
+    setTimeout(function () {
+      printLetters(phrase);
+    }, typingSpeed);
+  }
+}
+
+function clearLetters() {
+  if (letterIndex == -1) {
+    phraseIndex = (phraseIndex + 1) % phrases.length;
+    letterIndex = 0;
+    printLetters(phrases[phraseIndex]);
+  } else if (letterIndex > -1) {
+    let updatedPhrase = "";
+    for (let index = 0; index < letterIndex; index++) {
+      updatedPhrase += phrases[phraseIndex].charAt(index);
+    }
+    dynamicContent.textContent = updatedPhrase;
+    letterIndex -= 1;
+    setTimeout(clearLetters, erasingSpeed);
+  }
+}
+
+printLetters(phrases[phraseIndex]);
